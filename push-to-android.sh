5fbae24d4e00a98647d848086cf253b3fb120211
twobit <s8572327@gmail.com>
2016-02-14 23:42:42 +0100

    more comments

--- push-to-android.sh
+++ push-to-android.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
+# copy eveything to /sdcard/lovegame
 mkdir lovegame
 cp -r *.lua media/ lovegame/
 adb push lovegame/ /sdcard/lovegame
-exit
 
-
-zip -9 -q -r gorge.love *.lua media/
-adb push gorge.love /sdcard/Download/gorge.love
-adb shell am start -S -n "org.love2d.android/.GameActivity" -d "file:///sdcard/Download/gorge.love"
+#zip -9 -q -r gorge.love *.lua media/
+#adb push gorge.love /sdcard/Download/gorge.love
+#adb shell am start -S -n "org.love2d.android/.GameActivity" -d "file:///sdcard/Download/gorge.love"
