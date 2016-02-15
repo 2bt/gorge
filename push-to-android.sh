de84cafd30eab8e02e8454c85e7fe048ba5af831
twobit <s8572327@gmail.com>
2016-02-15 16:59:17 +0100

    remove tmp folder

--- push-to-android.sh
+++ push-to-android.sh
@@ -4,6 +4,7 @@
 mkdir lovegame
 cp -r *.lua media/ lovegame/
 adb push lovegame/ /sdcard/lovegame
+rm -r lovegame/
 
 #zip -9 -q -r gorge.love *.lua media/
 #adb push gorge.love /sdcard/Download/gorge.love
